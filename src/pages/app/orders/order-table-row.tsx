727218f44effd12f2fdf0808e2c8c448e8ddf56d
pedro felippe <pedfelippe@gmail.com>
2024-01-05 04:24:33 -0300

    feat: detalhes do dialog

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,26 +1,33 @@
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { ArrowRight, Search, X } from 'lucide-react';
+import { OrderDetails } from './order-details';
 
 function OrderTableRow() {
   return (
     <TableRow >
       <TableCell>
-        <Button variant="outline" size="sm">
-          <Search className='h-3 w-3'></Search>
-          <span className='sr-only'>Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="sm">
+              <Search className='h-3 w-3'></Search>
+              <span className='sr-only'>Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails/>
+        </Dialog>
       </TableCell>
 
       <TableCell className="font-mono text-xs font-medium">id0101101010</TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell className="text-muted-foreground"> 15 minutos</TableCell>
       <TableCell className="">
         <div className='flex items-center gap-2'>
           <span className='h-2 w-2 rounded-full bg-slate-400'></span>
           <span className='font-medium text-muted-foreground'>Pendentes</span>
         </div>
       </TableCell>
-      <TableCell className="font-medium">pedro felippe domingos fernandes</TableCell>
+      <TableCell className="font-medium">Pedro Felippe Domingos Fernandes</TableCell>
       <TableCell className="font-medium">R$ 190,00</TableCell>
       <TableCell className="">
         <Button variant="outline" size="sm">
