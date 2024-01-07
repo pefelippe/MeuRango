b69a9dd1d1a4c9e1c0d80c9b66a23eca0b7f8f79
pedro felippe <pedfelippe@gmail.com>
2024-01-07 04:49:25 -0300

    feat: finalizando ui. graficos done

--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -1,9 +1,11 @@
-
 import { Helmet } from "react-helmet-async"
-import DayOrdersAmountCard from "./DayOrdersAmountCard"
-import MonthOrdersCancelledCard from "./MonthCancelledOrdersCard"
-import MonthOrdersAmountCard from "./MonthOrdersAmountCard"
-import MonthRevenueCard from "./MonthRevenueCard"
+
+import DayOrdersAmountCard from "./cards/DayOrdersAmountCard"
+import MonthOrdersCancelledCard from "./cards/MonthCancelledOrdersCard"
+import MonthOrdersAmountCard from "./cards/MonthOrdersAmountCard"
+import MonthRevenueCard from "./cards/MonthRevenueCard"
+import { PopularProducts } from "./charts/PopularProducts"
+import { RevenueChart } from "./charts/RevenueChart"
 
 function Dashboard() {
   return (
@@ -17,6 +19,11 @@ function Dashboard() {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <MonthOrdersCancelledCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart/>
+          <PopularProducts />
         </div>
       </div>
     </div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
-import Dashboard from "./pages/app/dashboard/Dashboard";
+import { NotFound } from "./pages/404";
+import Dashboard from "./pages/app/dashboard";
 import Orders from "./pages/app/orders/orders";
 import { SignIn } from "./pages/auth/Sign-in";
 import { SignUp } from "./pages/auth/Sign-up";
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout/>,
+    errorElement: <NotFound />,
     children: [{
         path: "/",
         element: <Dashboard/>,
